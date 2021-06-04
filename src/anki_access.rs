f552dee2eed55b6a048f2fd38eed2e0b383b976a
jannes <jannes.dev@posteo.net>
2021-06-04 21:23:00 +0200

    Update for Anki 2.1.44
    Previously code was based of Anki 2.1.26 schema,
    since then notetypes and fields tables were added,
    which were previously in the cols table's models field as JSON.

--- src/anki_access.rs
+++ src/anki_access.rs
@@ -1,16 +1,12 @@
 extern crate rusqlite;
 extern crate serde_json;
 
-use anyhow::{Context, Result};
-use rusqlite::{params, Connection, Statement, ToSql, NO_PARAMS};
-use serde_json::Value;
-use std::collections::{HashMap, HashSet};
-
-#[derive(Debug)]
-pub struct Deck {
-    pub id: i64,
-    pub name: String,
-}
+use anyhow::Result;
+use rusqlite::{params, Connection, Statement, ToSql};
+use std::{
+    collections::{HashMap, HashSet},
+    convert::TryInto,
+};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub enum NoteStatus {
@@ -25,42 +21,30 @@ pub struct ZhNote {
     pub status: NoteStatus,
 }
 
-#[allow(dead_code)]
-pub fn get_decks(conn: &Connection) -> Result<Vec<Deck>> {
-    let mut example_query = conn.prepare("SELECT decks FROM col")?;
-    let mut cols = example_query.query_map(NO_PARAMS, |row| {
-        Ok(DecksWrapper {
-            json_str: row.get(0)?,
-        })
-    })?;
-    let only_row = cols.next().context("should have one row")?;
-    jsondecks_to_decks(only_row?.json_str).context("rusqlite error when parsing deck json")
-}
-
 pub fn get_zh_notes(
     conn: &Connection,
     note_field_map: &HashMap<&str, &str>,
 ) -> Result<HashSet<ZhNote>> {
-    let fields_info = get_zh_fields_info(conn, note_field_map)?;
+    let fields_info = get_zh_fields(conn, note_field_map)?;
     let mut all_notes: HashSet<ZhNote> = HashSet::new();
-    for i in 0..fields_info.amount {
-        let note_type_id = *fields_info.note_ids.get(i).unwrap();
-        let field_index = *fields_info.zh_field_indexes.get(i).unwrap() as usize;
+    for notetype_field in fields_info {
+        let notetype_id = notetype_field.notetype_id;
+        let field_index = notetype_field.field_order;
         all_notes.extend(select_notes(
             conn,
-            note_type_id,
+            notetype_id,
             field_index,
             NoteStatus::Active,
         )?);
         all_notes.extend(select_notes(
             conn,
-            note_type_id,
+            notetype_id,
             field_index,
             NoteStatus::SuspendedUnknown,
         )?);
         all_notes.extend(select_notes(
             conn,
-            note_type_id,
+            notetype_id,
             field_index,
             NoteStatus::SuspendedKnown,
         )?);
@@ -73,15 +57,11 @@ pub fn get_zh_notes(
 */
 
 #[derive(Debug)]
-struct DecksWrapper {
-    json_str: String,
-}
-
-#[derive(Debug)]
-struct ZhFieldsInfo {
-    note_ids: Vec<i64>,
-    zh_field_indexes: Vec<i64>,
-    amount: usize,
+struct NotetypeField {
+    notetype_id: i64,
+    notetype_name: String,
+    field_name: String,
+    field_order: usize,
 }
 
 const SELECT_ACTIVE_SQL: &str = "SELECT notes.flds FROM notes JOIN cards \
@@ -96,6 +76,10 @@ const SELECT_INACTIVE_SQL: &str = "SELECT notes.flds FROM notes JOIN cards \
             AND cards.queue = -1 \
             AND cards.flags = ?2 \
             AND cards.ord = 0";
+
+const SELECT_NOTETYPES_SQL: &str = "SELECT notetypes.id, notetypes.name, FIELDS.name, FIELDS.ord \
+            FROM notetypes JOIN FIELDS \
+            ON notetypes.id = FIELDS.ntid";
 
 fn select_notes(
     conn: &Connection,
@@ -130,17 +114,6 @@ fn select_notes(
     }
 }
 
-#[allow(dead_code)]
-/// given a vector of i64 e.g [i1, i2, i3] return single string "<i1>, <i2>, <i3>"
-fn id_vec_to_sql_set(ids: &[i64]) -> String {
-    match ids.get(0) {
-        Some(i) => ids[1..]
-            .iter()
-            .fold(i.to_string(), |acc, &i2| acc + ", " + &i2.to_string()),
-        None => String::new(),
-    }
-}
-
 /// get the field at given index from a Anki fields string
 /// Anki fields strings use 0x1f character as separator
 fn fieldsstr_to_field(fields: &str, index: usize) -> String {
@@ -149,69 +122,25 @@ fn fieldsstr_to_field(fields: &str, index: usize) -> String {
     String::from(*split.get(index).unwrap())
 }
 
-fn get_zh_fields_info(
+fn get_zh_fields(
     conn: &Connection,
     note_field_map: &HashMap<&str, &str>,
-) -> Result<ZhFieldsInfo> {
-    let mut notes_query = conn.prepare("SELECT models FROM col")?;
-    let mut rows = notes_query.query(NO_PARAMS)?;
-    let notes_json_str: String = rows.next()?.unwrap().get_unwrap(0);
-    let parsed_notes: Value = serde_json::from_str(notes_json_str.as_str()).unwrap();
-    let notes_map = parsed_notes
-        .as_object()
-        .context("this should be a json object")?;
-
-    let mut note_ids = Vec::new();
-    let mut zh_field_indexes = Vec::new();
-    let mut amount = 0;
-
-    // search for each target note and its associated field
-    for note_id in notes_map.keys() {
-        let note = notes_map.get(note_id).unwrap().as_object().unwrap();
-        let note_name = note.get("name").unwrap().as_str().unwrap();
-        if note_field_map.contains_key(note_name) {
-            let target_field = note_field_map.get(note_name).unwrap();
-            let fields = note.get("flds").unwrap().as_array().unwrap();
-            for field in fields {
-                let field_object = field.as_object().unwrap();
-                let field_name = field_object.get("name").unwrap().as_str().unwrap();
-                if *target_field == field_name {
-                    let note_id_int: i64 = note.get("id").unwrap().as_i64().unwrap();
-                    let field_index = field_object.get("ord").unwrap().as_i64().unwrap();
-                    note_ids.push(note_id_int);
-                    zh_field_indexes.push(field_index);
-                    amount += 1;
-                }
-            }
-        }
-    }
-    Ok(ZhFieldsInfo {
-        note_ids,
-        zh_field_indexes,
-        amount,
-    })
-}
-
-#[allow(dead_code)]
-fn jsondecks_to_decks(json_str: String) -> Result<Vec<Deck>, serde_json::Error> {
-    let parsed_json: Value = serde_json::from_str(json_str.as_str())?;
-    let arr = parsed_json.as_object();
-    match arr {
-        Some(elements) => elements.values().map(jsondeck_to_deck).collect(),
-        None => panic!("expected a valid json array of decks"),
-    }
-}
-
-#[allow(dead_code)]
-fn jsondeck_to_deck(json_deck: &Value) -> Result<Deck, serde_json::Error> {
-    Ok(Deck {
-        id: json_deck.get("id").unwrap().as_i64().unwrap(),
-        name: json_deck
-            .get("name")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .parse()
-            .unwrap(),
-    })
+) -> Result<Vec<NotetypeField>> {
+    let mut notetypes_query = conn.prepare(SELECT_NOTETYPES_SQL)?;
+    let notetype_fields = notetypes_query.query_map(params![], |row| {
+        let field_order: i64 = row.get(3)?;
+        Ok(NotetypeField {
+            notetype_id: row.get(0)?,
+            notetype_name: row.get(1)?,
+            field_name: row.get(2)?,
+            field_order: field_order.try_into().unwrap(),
+        })
+    })?;
+    Ok(notetype_fields
+        .map(|nf| nf.unwrap())
+        .filter(|nf| {
+            note_field_map.contains_key(nf.notetype_name.as_str())
+                && note_field_map.get(nf.notetype_name.as_str()).unwrap() == &nf.field_name
+        })
+        .collect())
 }
